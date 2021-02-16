acd2f584f5b573e27289644ee7be02f4549cda37
Natnael <it.natnael.zerihun@gmail.com>
2021-02-16 05:18:34 -0500

    imported necessary fuctions and brougth html tags on addUser.js

---
+++ assets/js/addUser.js
@@ -0,0 +1,11 @@
+//inputs and button indexPage
+let signupUsername = document.querySelector('.signupUsername');
+let signupemail = document.querySelector('.signupemail');
+let signuppassword = document.querySelector('.signuppassword');
+let signupconfirmpassword = document.querySelector('.signupconfirmpassword');
+let signupBtn = document.querySelector('.signupBtn');
+let loginModal = document.querySelector('.loginModal');
+let signupModal = document.querySelector('.signupModal');
+
+import * as addtoDB from './dataBaseInteraction.js';
+import * as helpers from './helper.js'
