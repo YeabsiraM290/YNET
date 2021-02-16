647e872f597e4dc43a7f3bf12ed2222f667e6535
Natnael <it.natnael.zerihun@gmail.com>
2021-02-16 07:40:07 -0500

    imported necessary fuctions and brougth html tags on login_validate.js

---
+++ assets/js/login_validate.js
@@ -0,0 +1,15 @@
+
+let loginBtn = document.querySelector('.loginBtn');
+let loginemail = document.querySelector('.loginemail');
+let loginpass = document.querySelector('.loginpass');
+let loginSubtitle = document.querySelector('.loginSubtitle');
+let loginModal = document.querySelector('#loginWindow');
+let vendor_balance = document.querySelector('.vendor_balance');
+let vendor_icon = document.querySelector('.vendorIcon');
+let car_icon = document.querySelector('.cartIcon');
+let profile = document.querySelector('.profile');
+let balance = "";
+let type = "";
+let who = "";
+
+import * as addtoDB from './dataBaseInteraction.js';
