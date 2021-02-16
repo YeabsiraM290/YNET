54ec504a6d2630af7184174dd54ee9da80442603
Natnael <it.natnael.zerihun@gmail.com>
2021-02-16 05:53:36 -0500

    Created userInfoValidation to add user info to database addUser.js

--- assets/js/addUser.js
+++ assets/js/addUser.js
@@ -9,3 +9,69 @@ let signupModal = document.querySelector('.signupModal');
 
 import * as addtoDB from './dataBaseInteraction.js';
 import * as helpers from './helper.js'
+if(signupBtn){ signupBtn.addEventListener('click', userInfoValidation);}
+
+function userInfoValidation(e){
+
+    e.preventDefault();
+  
+            let userName = helpers.checkLength(signupUsername.value,4);
+            let email = helpers.ValidateEmail(signupemail.value);
+            let pass = helpers.checkLength(signuppassword.value,8);
+            let confPass = helpers.comparePass(signuppassword.value, signupconfirmpassword.value);
+            
+            let succsess=0;
+
+            if(!userName){
+                signupUsername.style.borderWidth = "1px";
+                signupUsername.style.borderColor = "red";
+                signupUsername.value = "";
+                signupUsername.placeholder = "Username cannot be less than 4 chracters";
+                succsess=1;
+            }
+
+            if(!email){
+                
+                signupemail.style.borderWidth = "1px";
+                signupemail.style.borderColor = "red";
+                signupemail.value = "";
+                signupemail.placeholder = "Enter valid email address";
+                succsess=1;
+            }
+
+            if(!pass){
+                signuppassword.style.borderWidth = "1px";
+                signuppassword.style.borderColor = "red";
+                signuppassword.value = "";
+                signuppassword.placeholder = "Password must be at least 8 charcters";
+                succsess=1;
+            }
+
+            if(!confPass){
+
+                signupconfirmpassword.style.borderWidth = "1px";
+                signupconfirmpassword.style.borderColor = "red";
+                signupconfirmpassword.value = "";
+                succsess=1;
+            }
+
+            if(succsess==0){
+
+                let username = signupUsername.value;
+                let password = signuppassword.value;
+                let uEmail = signupemail.value;
+                let balance = '1000';
+                
+                signupModal.style.display = "none";
+                loginModal.style.display = "block";
+                
+                signupUsername.style.border = "none";
+                signupemail.style.border = "none";
+                signuppassword.style.border = "none";
+                signupconfirmpassword.style.border = "none";
+            
+                addtoDB.addNewuser(username, uEmail, password);
+                addtoDB.addTologin(username, uEmail, password, balance, "buyer");
+            
+            }
+}
