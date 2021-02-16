369099e59322177c670b298ca11ef187c20159cf
Natnael <it.natnael.zerihun@gmail.com>
2021-02-16 07:56:26 -0500

    Created loginValidationto add user info to database login_valodation.js

--- assets/js/login_validate.js
+++ assets/js/login_validate.js
@@ -13,3 +13,76 @@ let type = "";
 let who = "";
 
 import * as addtoDB from './dataBaseInteraction.js';
+
+
+if(loginBtn){loginBtn.addEventListener('click', loginValidation);}
+
+function loginValidation(e){
+
+    let email = loginemail.value;
+    let pass = loginpass.value;
+
+    let transaction = addtoDB.DB.transaction(['login'], 'readwrite');
+    let objectStore = transaction.objectStore('login');
+    let request = objectStore.openCursor();
+
+    request.onerror = function(event) {
+        console.err("error fetching data");
+    };
+
+    request.onsuccess = function(event) {
+    let cursor = event.target.result;
+
+        if (cursor) {
+
+            let uname = cursor.value.username;
+            let ema = cursor.value.email;
+            let pas = cursor.value.password;
+            
+            if(uname == email || ema == uname){
+
+                if(pass == pas){
+
+                    balance = cursor.value.balance;
+                    //who = cursor.value.username;
+                    
+                    type = cursor.value.user_type
+                    loginSubtitle.innerHTML = uname;
+
+                    vendor_icon.className = "nav-item ml-5 vendorIcon"
+                    car_icon.className = "nav-item ml-5 mt-2 cartIcon"
+                    vendor_balance.innerHTML = `Balance<br>${balance}`
+                    //loginModal.style.display = "none"
+                    loginemail.style.border = "none";
+                    loginpass.style.border = "none";
+
+                    if(type == "buyer"){
+
+                        profile.setAttribute("href", "buyerProfile.html");
+                    }
+
+                    else{
+
+                        profile.setAttribute("href", "sellerProfile.html");
+                    }
+
+                    return;
+                }
+            }
+
+            cursor.continue();
+        
+    }
+        else {
+            loginemail.style.borderColor = "red";
+            loginemail.style.borderWidth = "1px";
+            loginemail.value = "";
+
+            loginpass.style.borderColor = "red";
+            loginpass.style.borderWidth = "1px";
+            loginpass.value = ""; 
+            console.log("No")
+            return;
+        }
+    }
+}
